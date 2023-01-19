58b55924295e0529f9b295fd1c643a7c11e57507
therealdanmalmx <danmalmx@gmail.com>
2023-01-19 22:30:42 +0100

    Cusomized controller

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -2,6 +2,43 @@
  * post controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController(
+  "api::post.post",
+  ({ strapi }) => ({
+    // async find(ctx) {
+    //   const { data, meta } = await super.find(ctx);
+    //   if (ctx.state.user) {
+    //     return { data, meta };
+    //   }
+    //   const filterData = data.filter(
+    //     (post: { attributes: { premium: any } }) => !post.attributes.premium
+    //   );
+    //   return { data: filterData, meta };
+    // },
+
+    async find(ctx) {
+      console.log("ctx.query", ctx.query);
+      const isRequestingNonPremium =
+        ctx.query.filters &&
+        (ctx.query.filters as any).premium["$eq"] == "false";
+      if (ctx.state.user || isRequestingNonPremium) {
+        return await super.find(ctx);
+      }
+      const { query } = ctx;
+      const filteredPosts = strapi.service("api::post.post").find({
+        ...query,
+        filters: {
+          ...query.filters,
+          premium: false,
+        },
+      });
+      const sanitizedPosts = await this.sanitizeOutput(
+        filteredPosts,
+        ctx.state
+      );
+      return this.transformResponse(sanitizedPosts);
+    },
+  })
+);
